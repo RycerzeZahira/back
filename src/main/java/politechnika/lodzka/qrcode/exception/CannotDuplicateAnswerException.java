96177a1e520d1ed39076c63f00b0c7d46fc6bbce
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-27 16:24:41 +0200

    Klonowanie, update, sprawdzanie czy uzytkownik wypelnil formularz, weryfikacja daty formularza rzy dawaniu odpowiedzi - formularz

---
+++ src/main/java/politechnika/lodzka/qrcode/exception/CannotDuplicateAnswerException.java
@@ -0,0 +1,15 @@
+package politechnika.lodzka.qrcode.exception;
+
+import politechnika.lodzka.qrcode.model.Form;
+import politechnika.lodzka.qrcode.model.user.User;
+
+public class CannotDuplicateAnswerException extends AppException {
+    public CannotDuplicateAnswerException(User user, Form form) {
+        super(new StringBuilder()
+                .append("The user: ")
+                .append(user.getEmail())
+                .append(" has already given the answer for form: ")
+                .append(form.getCode())
+                .toString());
+    }
+}
